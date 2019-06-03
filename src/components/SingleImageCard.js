3ff7d4f01a45c43139f241b1c82ad275108352f2
Prashanth Goud Adurugatla <33069358+PrashanthAdurugatla@users.noreply.github.com>
2019-06-02 20:22:25 -0700

    Update Card styles

--- src/components/SingleImageCard.js
+++ src/components/SingleImageCard.js
@@ -6,21 +6,13 @@ const SingleImageCard = (props) => {
     const{posts,id} = props;
     return (
         <div>
-            <div className='container-fluid d-flex justify-content-center '>
-                <div className="row">
-                    <div className="m-auto">
-                        <div className="card-deck">
-                            <ImageCard
-                                key={id}
-                                img={posts[id].image}
-                                tag={posts[id].tag}
-                                date={posts[id].date}
-                                des={posts[id].description}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <ImageCard
+                key={id}
+                img={posts[id].image}
+                tag={posts[id].tag}
+                date={posts[id].date}
+                des={posts[id].description}
+            />
         </div>
     );
 }
