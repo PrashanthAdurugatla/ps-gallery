b2df2942c6ce1c764f381ad25af6075ca753b057
Prashanth Goud Adurugatla <33069358+PrashanthAdurugatla@users.noreply.github.com>
2019-06-02 20:20:29 -0700

    Update Card styles

--- src/components/ImageCardList.js
+++ src/components/ImageCardList.js
@@ -6,24 +6,16 @@ const ImageCardList = (props) => {
     const cardComponentArray = posts.slice(0,4);
     return (
         <div>
-            <div className="container mt-5 ">
-                <div className="row">
-                    <div className="col">
-                        <div className="card-deck ">
-                            {cardComponentArray.map((image, i) =>{
-                                return (
-                                    <ImageCard
-                                        key={i}
-                                        img={posts[i].image}
-                                        tag={posts[i].tag}
-                                        date={posts[i].date}
-                                        des={posts[i].description} />
-                                );
-                            })}
-                        </div>
-                    </div>
-                </div>
-            </div>
+            {cardComponentArray.map((image, i) =>{
+                return (
+                    <ImageCard
+                        key={i}
+                        img={posts[i].image}
+                        tag={posts[i].tag}
+                        date={posts[i].date}
+                        des={posts[i].description} />
+                );
+            })}
         </div>
     );
 }
